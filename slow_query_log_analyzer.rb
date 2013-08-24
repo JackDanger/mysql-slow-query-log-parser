241890c7064e42046a5fa9e3962c3936e22f7985
Jack Danger Canty <jackdanger@squareup.com>
2013-08-24 15:56:44 -0700

    Better examples (from actual usage)

--- slow_query_log_analyzer.rb
+++ slow_query_log_analyzer.rb
@@ -3,10 +3,23 @@
 #
 #   require 'slow_query_log_analyzer'
 #   queries = SlowLogAnalyzer.new('/data/fusionio/mysql/slow-query.log').queries
+#   # Gated by a specific minimum slowness:
+#   queries.slower_than(5).first
+#
+#   # From an interesting file:
+#   queries.from_file(/_job\.rb/).map {|q| q[:Query_time] }
+#
+#   # An arbitrarily complex (still lazily-processed!) query to show
+#   # the mean time taken for any given line of source code
 #   queries.slower_than(5).select do |q|
 #      q[:source]
-#   end.from_file(/my_slow_controller.rb:123/).drop(5).map do |q|
-#     q.select {|k,v| [:sql, :source, 'Query_time'].include?(k) }
+#   end.from_file(/my_slow_controller.rb:123/).drop(5).
+#   group_by {|q| q[:source] }.map do |source, qq|
+#     qq.reduce(n: 0.1, total: 0, source: source) do |data, q|
+#       data.update n: data[:n]+1, total: data[:total]+q['Query_time']
+#     end
+#   end.sort_by {|data| data[:total]/data[:n] }.reverse.each do |data|
+#     puts "#{'%0.4f' % (data[:total]/data[:n])} #{data[:source]}"
 #   end
 #
 class SlowQueryLogAnalyzer
